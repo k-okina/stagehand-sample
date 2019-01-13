02b7579ef77f7254eec8354cc748c703f9753047
k-okina <okinakahiro@gmail.com>
2019-01-13 13:55:14 +0900

    initialize

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:stagehand_sample/stagehand_sample.dart' as stagehand_sample;
+
+main(List<String> arguments) {
+  print('Hello world: ${stagehand_sample.calculate()}!');
+}
---
+++ test/stagehand_sample_test.dart
@@ -0,0 +1,8 @@
+import 'package:stagehand_sample/stagehand_sample.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
